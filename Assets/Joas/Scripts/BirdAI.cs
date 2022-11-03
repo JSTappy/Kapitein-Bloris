33440293c873c49b9475167b7d68aa2f859c8719
KoenBos <koenbos@outlook.com>
2022-11-03 11:29:00 +0100

    Vogel ai

--- Assets/Joas/Scripts/BirdAI.cs
+++ Assets/Joas/Scripts/BirdAI.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,24 @@ using UnityEngine;
 public class BirdAI : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.125f;
+    private float smoothSpeed = 0.1f;
     public Vector3 offset;
+
+    void Start(){
+        transform.rotation = new Vector3  (0.0f, 90.0f, 0.0f);
+    }
+    
 
     void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        //Vector3 desiredPosition = target.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //transform.position = smoothedPosition;
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position -= new Vector3(smoothSpeed, 0f, 0f);
     }
 }
